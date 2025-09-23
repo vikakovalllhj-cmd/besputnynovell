733b0419f2cb48e84149d4919bb85f6cfba07c6b
vikakovalllhj-cmd <vikakovalllhj@gmail.com>
2025-09-23 23:11:40 +0300

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,4 +0,0 @@
-// Конфиг для Supabase
-const SUPABASE_URL = 'https://rzooujinsekmgswgbixv.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ6b291amluc2VrbWdzd2diaXh2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg2Mzg3NzIsImV4cCI6MjA3NDIxNDc3Mn0.41o_lwuKZRJENtOtYMVpnZNE7mOz-TJiclCnK_0g08I';
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
