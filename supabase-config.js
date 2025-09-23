6d0ffabbae31d26293708bf9118b76074f1e468e
vikakovalllhj-cmd <vikakovalllhj@gmail.com>
2025-09-23 20:36:02 +0300

    Create supabase-config.js

---
+++ supabase-config.js
@@ -0,0 +1,6 @@
+// Конфигурация для Supabase
+const SUPABASE_URL = "https://rzooujinsekmgswgbixv.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ6b291amluc2VrbWdzd2diaXh2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg2Mzg3NzIsImV4cCI6MjA3NDIxNDc3Mn0.41o_lwuKZRJENtOtYMVpnZNE7mOz-TJiclCnK_0g08I";
+
+// Создаём объект для работы с базой
+const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
