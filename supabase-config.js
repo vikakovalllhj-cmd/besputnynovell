9322631c35ca77a3091a8dadcb8ad72202893590
vikakovalllhj-cmd <vikakovalllhj@gmail.com>
2025-09-24 02:02:14 +0300

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,7 +0,0 @@
-// supabase-config.js
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
-
-const SUPABASE_URL = "https://rzooujinsekmgswgbixv.supabase.co"
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ6b291amluc2VrbWdzd2diaXh2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg2Mzg3NzIsImV4cCI6MjA3NDIxNDc3Mn0.41o_lwuKZRJENtOtYMVpnZNE7mOz-TJiclCnK_0g08I"
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
